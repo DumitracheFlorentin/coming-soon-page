4b4b4176cc0a0ade2015b8055640cfb13d76efb5
Florentin Dumitrache <dumitracheflorentincristian@gmail.com>
2021-09-14 14:00:37 +0300

    Done the functionality part

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,13 @@
+const submitBtn = document.querySelector(".hero__form__btn");
+const emailInput = document.querySelector(".hero__form__input");
+const errorMessage = document.querySelector(".hero__form__error");
 
+submitBtn.addEventListener("click", () => {
+  // You can include your own regex for email validation, this is only a test
+  if (emailInput.value === "" || !(emailInput.value.indexOf("@") > -1)) {
+    console.log(errorMessage);
+    errorMessage.style.display = "block";
+    emailInput.removeAttribute("placeholder");
+    emailInput.style.borderColor = "var(--light-red)";
+  }
+});
